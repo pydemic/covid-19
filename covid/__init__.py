ae0b6cbe5b084379068c7652dd9d5fb07957eb84
Fábio Macêdo Mendes <fabiomacedomendes@gmail.com>
2020-04-03 09:28:48 -0300

    Fix some simple translation mistakes

--- covid/__init__.py
+++ covid/__init__.py
@@ -5,18 +5,35 @@ Implements epidemiological simulations for the COVID-19.
 """
 __version__ = "0.1.0"
 __author__ = "Fábio Mendes"
-import gettext as _gettext
 from pathlib import Path as _Path
 
-from .region import Region, region
 from . import models
+from .region import Region, region
 
 LOCALEDIR = _Path(__file__).parent / "locale"
 
 
 def set_i18n(code):
-    _gettext.bindtextdomain(code, localedir=LOCALEDIR)
+    import gettext
+
+    gettext.bindtextdomain(code, localedir=LOCALEDIR)
 
 
-set_i18n("messages")
-gettext = _gettext.gettext
+def _run():
+    import os
+    import locale
+
+    # Use environment variables to set i18n and l13n configurations.
+    # LANGUAGE -> language part of the locale, e.g., pt_BR
+    # LANG -> full locale string, e.g., pt_BR.UTF-8
+    language = os.environ.get("LANGUAGE", "messages")
+    language = os.environ.get("COVID_LANGUAGE", language)
+    set_i18n(language)
+
+    lang = os.environ.get("COVID_LANG") or os.environ.get("LANG")
+    if lang:
+        locale.setlocale(locale.LC_ALL, lang)
+
+
+_run()
+del _run, _Path
