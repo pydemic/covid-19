8f52b53b1a3cd3cf978f2b9de5eb10fb6d2674e8
Fábio Macêdo Mendes <fabiomacedomendes@gmail.com>
2020-04-04 12:58:05 -0300

    Add calc_pt_BR that forces a localized version

--- covid/__init__.py
+++ covid/__init__.py
@@ -5,39 +5,47 @@ Epidemiological calculator tuned specifically for COVID-19.
 """
 __version__ = "0.1.0"
 __author__ = "Fábio Mendes"
+from gettext import gettext
 from pathlib import Path as _Path
 
 from . import models
 from .region import Region, region
-from gettext import gettext
 
 LOCALEDIR = _Path(__file__).parent / "locale"
 
 
-def set_i18n(code):
-    import gettext
+def set_i18n(lang, language=...):
+    """
+    Set locale and translations.
 
-    gettext.bindtextdomain(code, localedir=LOCALEDIR)
+    Examples:
+        set_i18n('pt_BR.UTF-8') -> set locale to pt_BR.UTF-8 and language to pt_BR.
+    """
+    import gettext
+    import locale
+    import warnings
+
+    if language is ...:
+        language = lang.partition(".")[0]
+    if language:
+        gettext.bindtextdomain(language or "messages", localedir=LOCALEDIR)
+    try:
+        locale.setlocale(locale.LC_ALL, lang)
+    except locale.Error:
+        warnings.warn(f"locale is not supported: {lang}")
 
 
 def _run():
     import os
-    import locale
-    import warnings
 
     # Use environment variables to set i18n and l13n configurations.
     # LANGUAGE -> language part of the locale, e.g., pt_BR
     # LANG -> full locale string, e.g., pt_BR.UTF-8
     language = os.environ.get("LANGUAGE", "messages")
     language = os.environ.get("COVID_LANGUAGE", language)
-    set_i18n(language)
 
     lang = os.environ.get("COVID_LANG") or os.environ.get("LANG")
-    if lang:
-        try:
-            locale.setlocale(locale.LC_ALL, lang)
-        except locale.Error:
-            warnings.warn(f"locale is not supported: {lang}")
+    set_i18n(lang, language)
 
 
 _run()
---
+++ covid/ui/i18n/calc_pt_BR.py
@@ -0,0 +1,6 @@
+from covid.ui import calc
+from covid import set_i18n
+
+set_i18n("pt_BR.UTF-8", "pt_BR")
+ui = calc.CalcUI()
+ui.run()
