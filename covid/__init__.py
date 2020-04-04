1361e6063f316517f8ade8b76703a6e165b42b30
Fábio Macêdo Mendes <fabiomacedomendes@gmail.com>
2020-04-04 14:14:45 -0300

    Fix i18n problems?

--- covid/__init__.py
+++ covid/__init__.py
@@ -14,7 +14,7 @@ from .region import Region, region
 LOCALEDIR = _Path(__file__).parent / "locale"
 
 
-def set_i18n(lang, language=...):
+def set_i18n(lang, language=None):
     """
     Set locale and translations.
 
@@ -24,28 +24,23 @@ def set_i18n(lang, language=...):
     import gettext
     import locale
     import warnings
+    import os
 
-    if language is ...:
-        language = lang.partition(".")[0]
-    if language:
-        gettext.bindtextdomain(language or "messages", localedir=LOCALEDIR)
     try:
         locale.setlocale(locale.LC_ALL, lang)
+        locale.setlocale(locale.LC_MESSAGES, language or lang)
+        os.environ["LANG"] = lang
+        os.environ["LANGUAGE"] = language or lang.split(".")[0]
     except locale.Error:
         warnings.warn(f"locale is not supported: {lang}")
+    gettext.bindtextdomain("messages", localedir=LOCALEDIR)
 
 
 def _run():
     import os
 
-    # Use environment variables to set i18n and l13n configurations.
-    # LANGUAGE -> language part of the locale, e.g., pt_BR
-    # LANG -> full locale string, e.g., pt_BR.UTF-8
-    language = os.environ.get("LANGUAGE", "messages")
-    language = os.environ.get("COVID_LANGUAGE", language)
-
     lang = os.environ.get("COVID_LANG") or os.environ.get("LANG")
-    set_i18n(lang, language)
+    set_i18n(lang)
 
 
 _run()
--- covid/ui/i18n/calc_en_US.py
+++ covid/ui/i18n/calc_en_US.py
@@ -1,6 +1,6 @@
 from covid import set_i18n
 
-set_i18n("pt_BR.UTF-8")
+set_i18n("en_US.UTF-8")
 from covid.ui import calc
 
 ui = calc.CalcUI()
