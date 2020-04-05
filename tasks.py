6ca2ac0bec42b85070888fd0f5369cef771a935d
Jonathan Henrique Maia de Moraes <altjohndev@gmail.com>
2020-04-05 12:58:51 -0300

    Create task to sync streamlit i18n forks
    --update-development-image
    
    --update-production-image

--- tasks.py
+++ tasks.py
@@ -7,6 +7,30 @@ def calculator(ctx):
 
 
 @task
+def calculator_production(ctx):
+    import os
+
+    if os.environ.get("COVID_PERFORM_FIRST_SETUP", "false") != "true":
+        repos = {
+            "pt_BR.UTF-8": "https://github.com/caiosba/streamlit.git",
+            "en_US.UTF-8": "https://github.com/streamlit/streamlit.git",
+        }
+
+        lang = os.environ.get("COVID_LANG", "en_US.UTF-8")
+        repo = repos[lang]
+
+        path = "/usr/local/lib/python3.8/site-packages/streamlit"
+
+        ctx.run(f"cd {path} && git remote add {lang} {repo}")
+        ctx.run(f"cd {path} && git fetch {lang}")
+        ctx.run(f"cd {path} && git checkout {lang}/master")
+        ctx.run(f"cd {path}/frontend && yarn install")
+        ctx.run(f"cd {path} && make frontend")
+
+    ctx.run("inv calculator")
+
+
+@task
 def compilemessages(ctx):
     ctx.run("pybabel compile -d covid/locale")
 
