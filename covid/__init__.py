b4f81c17358484813b44f064da474bd78bc889ce
Fábio Macêdo Mendes <fabiomacedomendes@gmail.com>
2020-05-18 11:19:55 -0300

    Bump version numbers

--- covid/__init__.py
+++ covid/__init__.py
@@ -3,7 +3,7 @@ Covid
 
 Epidemiological calculator tuned specifically for COVID-19.
 """
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 __author__ = "Fábio Mendes"
 from gettext import gettext
 from pathlib import Path as _Path
